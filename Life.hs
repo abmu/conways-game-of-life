c6be5bcca55d31e7b0173d0bca204a1b3e63179c
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-08-24 12:37:03 +0100

    Added live neighbours and candidates functions

--- Life.hs
+++ Life.hs
@@ -1,3 +1,4 @@
+import qualified Data.Set as S
 import Data.Set (Set)
 
 type Cell = (Int, Int)
@@ -6,3 +7,10 @@ type Board = Set Cell
 
 neighbours :: Cell -> [Cell]
 neighbours (x,y) = [(x+dx, y+dy) | dx <- [-1..1], dy <- [-1..1], (dx,dy) /= (0,0)]
+
+liveNeighbours :: Board -> Cell -> Int
+liveNeighbours b c = length $ filter (`S.member` b) (neighbours c)
+
+-- Candidates for the next generation are every live cell, and their neighbours
+candidates :: Board -> Set Cell
+candidates b = S.unions $ map (\c -> S.fromList (c : neighbours c)) (S.toList b)
