998b9232667b57e07f7bc240c13af5ff850dafe5
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-08-24 15:21:10 +0100

    Added conway's step function and convert cell to gloss picture

--- Life.hs
+++ Life.hs
@@ -1,12 +1,12 @@
 import qualified Data.Set as S
 import Data.Set (Set)
+import Graphics.Gloss
 
 type Cell = (Int, Int)
-
 type Board = Set Cell
 
 neighbours :: Cell -> [Cell]
-neighbours (x,y) = [(x+dx, y+dy) | dx <- [-1..1], dy <- [-1..1], (dx,dy) /= (0,0)]
+neighbours (x, y) = [(x+dx, y+dy) | dx <- [-1..1], dy <- [-1..1], (dx, dy) /= (0,0)]
 
 liveNeighbours :: Board -> Cell -> Int
 liveNeighbours b c = length $ filter (`S.member` b) (neighbours c)
@@ -14,3 +14,14 @@ liveNeighbours b c = length $ filter (`S.member` b) (neighbours c)
 -- Candidates for the next generation are every live cell, and their neighbours
 candidates :: Board -> Set Cell
 candidates b = S.unions $ map (\c -> S.fromList (c : neighbours c)) (S.toList b)
+
+-- Conway's step
+step :: Board -> Board
+step b = S.fromList [c | c <- S.toList (candidates b), let n = liveNeighbours b c, (S.member c b && (n == 2 || n == 3)) || (not (S.member c b) && (n == 3))]
+
+-- Convert board to gloss pictures
+cellSize :: Float
+cellSize = 10
+
+cellToPicture :: Cell -> Picture
+cellToPicture (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) $ color white $ rectangleSolid cellSize cellSize 
