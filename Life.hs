c4e1cca460f3295cf7c08908cf705036fcaa13e6
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-08-24 15:50:12 +0100

    Added world data type and update world function

--- Life.hs
+++ Life.hs
@@ -5,6 +5,8 @@ import Graphics.Gloss
 type Cell = (Int, Int)
 type Board = Set Cell
 
+data World = World { board :: Board, paused :: Bool }
+
 neighbours :: Cell -> [Cell]
 neighbours (x, y) = [(x+dx, y+dy) | dx <- [-1..1], dy <- [-1..1], (dx, dy) /= (0,0)]
 
@@ -19,9 +21,17 @@ candidates b = S.unions $ map (\c -> S.fromList (c : neighbours c)) (S.toList b)
 step :: Board -> Board
 step b = S.fromList [c | c <- S.toList (candidates b), let n = liveNeighbours b c, (S.member c b && (n == 2 || n == 3)) || (not (S.member c b) && (n == 3))]
 
+updateWorld :: Float -> World -> World
+updateWorld _ w
+    | paused w = w
+    | otherwise = w { board = step (board w) }
+
 -- Convert board to gloss pictures
 cellSize :: Float
 cellSize = 10
 
 cellToPicture :: Cell -> Picture
-cellToPicture (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) $ color white $ rectangleSolid cellSize cellSize 
+cellToPicture (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) $ color white $ rectangleSolid cellSize cellSize
+
+boardToPicture :: Board -> Picture
+boardToPicture b = pictures $ map cellToPicture (S.toList b)
